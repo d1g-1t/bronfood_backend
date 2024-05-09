f90cd359ec76126824a342b12328758795e4b174
Pavel Okhrim <paokhrim@gmail.com>
2024-05-09 21:17:00 +0300

    Добавлены тесты моделей Class Restaurant, Class Meal и доступности эндпоинта /api/meals/

--- src/tests/api/restaurant/test_restaurants.py
+++ src/tests/api/restaurant/test_restaurants.py
@@ -19,3 +19,12 @@ class UrlTests(TestCase):
         guest_client = Client()
         response = guest_client.get("/api/menus/")
         self.assertEqual(response.status_code, 200)
+
+    def test_get_meals(self):
+        """
+        Тест проверки получения статуса 200 при get запросе
+        на адрес '/meals/'
+        """
+        guest_client = Client()
+        response = guest_client.get("/api/meals/")
+        self.assertEqual(response.status_code, 200)
