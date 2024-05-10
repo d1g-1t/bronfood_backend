ea117185eae895c9f1b16696d1d3983081f9b1fc
Alexander Solodnikov <solodnikov@yandex.ru>
2024-05-10 14:51:33 +0300

    settings debug False

--- src/bronfood/settings.py
+++ src/bronfood/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.getenv('DEBUG')
 DEBUG = False
-# ENV_NAME = os.getenv('ENV_NAME', 'local')
+ENV_NAME = os.getenv('ENV_NAME', 'local')
 
 
 ALLOWED_HOSTS = ['*']
