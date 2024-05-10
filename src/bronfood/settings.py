913e9638c2c0eba89fe520923117c435fc1b34e0
Alexander Solodnikov <solodnikov@yandex.ru>
2024-05-10 14:48:55 +0300

    settings debug False

--- src/bronfood/settings.py
+++ src/bronfood/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
-
-ENV_NAME = os.getenv('ENV_NAME', 'local')
+# DEBUG = os.getenv('DEBUG')
+DEBUG = False
+# ENV_NAME = os.getenv('ENV_NAME', 'local')
 
 
 ALLOWED_HOSTS = ['*']
