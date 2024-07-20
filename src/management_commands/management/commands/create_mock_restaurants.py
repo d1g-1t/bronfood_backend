f1afd3c2aa6df3cd7fdf9c1342a672282da8b3f9
Павел Охрим <paokhrim@gmail.com>
2024-07-20 08:33:55 +0300

    По просьбе фронтенда использованы другие данные для создания моковых ресторанов.

--- src/management_commands/management/commands/create_mock_restaurants.py
+++ src/management_commands/management/commands/create_mock_restaurants.py
@@ -1,95 +1,82 @@
 import logging
-import random
-
 from django.core.management.base import BaseCommand
+from django.db import transaction
+from bronfood.core.restaurants.models import Restaurant, Coordinates
 
-from bronfood.core.restaurants.models import (
-    Coordinates, Meal, Menu, Restaurant, Tag
-)
-from ._variables import (
-    COUNT_MOCK_DATA, MENU_CATEGORIES, RESTAURANT_TAGS, RESTAURANT_NAMES,
-    RESTAURANT_ADDRESSES, RESTAURANT_DESCRIPTIONS, BEGIN_TIMES, END_TIMES,
-    PATH_TO_RESTAURANTS_IMAGE_DIR, PATH_TO_MEDIA_PICS_FOLDER
-)
-from ._utils import (
-    random_string, get_random_image, create_meals, count_validate
-)
+logging.basicConfig(level=logging.INFO)
+
+IMAGE_PATH = '/src/management_commands/images/'
+
+MOCK_RESTAURANTS_DATA = [
+    {
+        "name": "Jahu",
+        "photo": "restaurant3.png",
+        "rating": 4.8,
+        "address": "ул. Березовая 21",
+        "latitude": 43.243523441782585,
+        "longitude": 76.91477137561034,
+        "workingTime": "09:00 - 22.00",
+        "type": "cafe",
+    },
+    {
+        "name": "Boom",
+        "photo": "restaurant2.png",
+        "rating": 4.9,
+        "address": "ул. Морозова 56/1",
+        "latitude": 43.239536903817104,
+        "longitude": 76.9312294101257,
+        "workingTime": "10:00 - 23.00",
+        "type": "cafe",
+    },
+    {
+        "name": "Moon",
+        "photo": "restaurant4.png",
+        "rating": 5.0,
+        "address": "пр. Мира 36",
+        "latitude": 43.23818774310171,
+        "longitude": 76.9074543094177,
+        "workingTime": "12:00 - 01.00",
+        "type": "cafe",
+    },
+    {
+        "name": "Ready",
+        "photo": "restaurant1.png",
+        "rating": 4.8,
+        "address": "ул. Березовая 21",
+        "latitude": 43.23531675447601,
+        "longitude": 76.9158273458476,
+        "workingTime": "08:00 - 20.00",
+        "type": "cafe",
+    },
+]
 
 
-def create_menu(meals, restaurants_count=COUNT_MOCK_DATA):
-    """Создаёт несколько моковых меню в базе"""
-    menu = [
-        Menu(
-            category=random_string(MENU_CATEGORIES)
+def create_mock_restaurants():
+    """Создает моковые рестораны в базе данных."""
+    for restaurant_data in MOCK_RESTAURANTS_DATA:
+        photo_path = IMAGE_PATH + restaurant_data["photo"]
+        coordinates = Coordinates.objects.create(
+            latitude=restaurant_data["latitude"],
+            longitude=restaurant_data["longitude"]
         )
-        for i in range(int(restaurants_count))
-    ]
-    menu_in_bd = Menu.objects.bulk_create(menu)
-    for menu in menu_in_bd:
-        menu.meals.add(*meals)
-    logging.info(f'Создано {str(restaurants_count)} меню.')
-    return menu_in_bd
-
-
-def create_restaurants(count=COUNT_MOCK_DATA, meal_count=COUNT_MOCK_DATA):
-    """Создаёт несколько моковых ресторанов в базе"""
-
-    count_validate(count)
-
-    tag, created = Tag.objects.get_or_create(name=random_string(RESTAURANT_TAGS))
-
-    if meal_count > Meal.objects.all().count():
-        meals = create_meals(meal_count)
-    else:
-        meals = Meal.objects.all()[:meal_count]
-
-    menu = create_menu(meals, count)
-
-    restaurants = [
-        Restaurant(
-            name=random_string(RESTAURANT_NAMES),
-            photo=get_random_image(
-                PATH_TO_RESTAURANTS_IMAGE_DIR, PATH_TO_MEDIA_PICS_FOLDER
-            ),
-            address=random_string(RESTAURANT_ADDRESSES),
-            coordinates=Coordinates.objects.create(
-                latitude=11.111111,
-                longitude=22.222222
-            ),
-            rating=random.randint(1, 5),
-            workingTime=f"{random_string(BEGIN_TIMES)} - {random_string(END_TIMES)}",
-            type=random.choice(
-                [choice[0] for choice in Restaurant.RESTAURANT_TYPES]
-            ),
+        Restaurant.objects.create(
+            name=restaurant_data["name"],
+            photo=photo_path,
+            rating=restaurant_data["rating"],
+            address=restaurant_data["address"],
+            coordinates=coordinates,
+            workingTime=restaurant_data["workingTime"],
+            type=restaurant_data["type"],
         )
-        for i in range(count)
-    ]
-    restaurants_in_bd = Restaurant.objects.bulk_create(restaurants)
-    for restaurant, menu_item in zip(restaurants_in_bd, menu):
-        restaurant.meals.add(*menu_item.meals.all())
-    logging.info(f'Создано {str(count)} ресторанов.')
-    return restaurants_in_bd
 
 
 class Command(BaseCommand):
-    help = 'Создаёт моковых Ресторанов.'
+    help = 'Создает моковые рестораны в базе данных.'
 
     def handle(self, *args, **options):
         try:
-            create_restaurants(options.get('count'), options.get('meal_count'))
+            with transaction.atomic():
+                create_mock_restaurants()
+                logging.info('Моковые рестораны успешно созданы.')
         except Exception as e:
-            return logging.error(e)
-
-    def add_arguments(self, parser):
-        parser.add_argument(
-            '--count',
-            type=int,
-            default=COUNT_MOCK_DATA,
-            help='Добавляет заданное колличество ресторанов в базу.'
-        )
-        parser.add_argument(
-            '--meal_count',
-            type=int,
-            default=COUNT_MOCK_DATA,
-            help='Колличество блюд в меню для каждого ресторана.'
-        )
+            logging.error(f'Ошибка при создании моковых ресторанов: {e}')
